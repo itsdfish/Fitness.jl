70c68bff4ae7fc960833081007d6e2ac6966b3a4
itsdfish <itsdfish@gmail.com>
2020-09-14 11:21:51 -0400

    update

--- src/Fitness.jl
+++ src/Fitness.jl
@@ -1,10 +1,11 @@
 module Fitness
     using BlackBoxOptim
     const Scheme = EpsBoxDominanceFitnessScheme
-    import BlackBoxOptim: add_candidate!
+    import BlackBoxOptim: add_candidate!, noprogress_streak, notify!
     export Scheme, EpsBoxArchive, pareto_frontier
     export best_candidate, add_candidate!, best_fitness
     export get_frontier, get_best_paths, get_best_cost
+    export noprogress_streak, notify!
 
     include("utilities.jl")
 end
